5710c6d57f648af48769c021eb4ff50d40190133
Bastrykov Evgeniy <vporoshok@gmail.com>
2014-02-21 09:51:17 +0400

    test with tox and travis

--- setup.py
+++ setup.py
@@ -4,12 +4,6 @@
 from setuptools import setup
 
 
-def read(fname):
-    # makes sure that setup can be executed from a different location
-    import os.path
-    _here = os.path.abspath(os.path.dirname(__file__))
-    return open(os.path.join(_here, fname)).read()
-
 setup(
     name='testserver',
     version='0.01-b0002',
@@ -18,7 +12,6 @@ setup(
     license='MIT License',
     description='''Threaded daemon localhost http server. Simple HEAD, GET,
                 PUT and POST request parsing for unittest.''',
-    long_description=read('README.md'),
     url='https://github.com/vporoshok/testserver',
     download_url='https://github.com/vporoshok/testserver/releases',
     zip_safe=False,
@@ -29,9 +22,7 @@ setup(
         'Development Status :: 1 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Topic :: Software Development :: Testing'
     ]
 )
